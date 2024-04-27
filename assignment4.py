c9bc0d170d980a96bc352dde458850c2ca4f0ada
PENIEL <peniel@PENIELs-MacBook-Air.local>
2024-04-27 14:34:17 -0400

    first commit

---
+++ assignment4.py
@@ -0,0 +1,12 @@
+#Task2
+Firstname = "Peniel"
+Middlename = "Ojonimi"
+Lastname = "Christopher"
+
+Fullname = Firstname +" "+ Middlename +" "+ Lastname 
+print(Fullname)
+
+#task3
+y = ["Firstname, Middlename, Lastname"]
+type_y = type(y)
+print(f"The value of  y is {y}, and the data type of y is {type_y}")
