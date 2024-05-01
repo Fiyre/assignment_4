adeb3cb9b07e2326a963207668ebda40bd15849f
PENIEL <peniel@PENIELs-MacBook-Air.local>
2024-05-01 12:01:56 -0400

    final commit

--- assignment4.py
+++ assignment4.py
@@ -6,7 +6,47 @@ Lastname = "Christopher"
 Fullname = Firstname +" "+ Middlename +" "+ Lastname 
 print(Fullname)
 
-#task3
+#List 
 y = ["Firstname, Middlename, Lastname"]
 type_y = type(y)
 print(f"The value of  y is {y}, and the data type of y is {type_y}")
+
+#Str
+x = "type_x"
+type_x = type(x)
+print(f"The value of x is {x}, and the data type of y is {type_x}")
+
+#Boolean 
+z = True
+type_z = type(z)
+print(f"The value of z is {z}, and the data type of z is {type_z}")
+
+#Set
+a = {"gender", "age", "name"}
+type_a = type(a)
+print(f"The value of a is {a}, and the data type of a is {type_a}")
+
+#Dict
+c = {"brand": "Ford", "model": "Mustang", "year": 1964}
+type_c = type(c)
+print(f"The value of c is {c}, and the data type of c is {type_c}")
+
+#turple
+d = ("Peniel", 11, "male")
+type_d = type(d)
+print(f"The value of d is {d}, and the data type of d is {type_d}")
+
+#float
+e = float(2)
+type_e = type(e)
+print(f"The value of e is {e}, and the data type of e is {type_e}")
+
+#Int
+f = int(4.5)
+type_f = type(f)
+print(f"The value of f is {f}, and the data type of f is {type_f}")
+
+#Complex
+g = complex(3, 5)
+type_g =  type(g)
+print(f"The value of g is {g}, and the data type of g is {type_g}")
